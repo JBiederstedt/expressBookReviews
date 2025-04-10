64cbd0cbb7b29bd14f41bc5acd5ef36d600e24a4
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-10 13:20:16 +0200

    Add delete book review for authenticated users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -65,6 +65,24 @@ regd_users.put('/auth/review/:isbn', (req, res) => {
   }
 });
 
+// Delete a book review
+regd_users.delete('/auth/review/:isbn', (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.user.username;
+
+  let book = books[isbn];
+  if (book) {
+    if (book.reviews[username]) {
+      delete book.reviews[username];
+      return res.status(200).json({ message: 'Review deleted successfully' });
+    } else {
+      return res.status(404).json({ message: 'Review by this user not found' });
+    }
+  } else {
+    return res.status(404).json({ message: 'Book not found' });
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
