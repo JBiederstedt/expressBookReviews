f026c98e9fe95c09ab40d2e1423da493c99ff55d
Jascha-Biederstedt <j.biederstedt@gmx.de>
2025-04-10 13:14:55 +0200

    Add book review for authenticated users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,8 +46,23 @@ regd_users.post('/login', (req, res) => {
 
 // Add a book review
 regd_users.put('/auth/review/:isbn', (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: 'Yet to be implemented' });
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+  const username = req.user.username;
+
+  if (!review) {
+    return res.status(400).json({ message: 'Review is required' });
+  }
+
+  let book = books[isbn];
+  if (book) {
+    book.reviews[username] = review;
+    return res
+      .status(200)
+      .json({ message: 'Review added/updated successfully' });
+  } else {
+    return res.status(404).json({ message: 'Book not found' });
+  }
 });
 
 module.exports.authenticated = regd_users;
